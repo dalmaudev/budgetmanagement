f2f1c30876b1c0fd2836701999def139290fecef
Jordi Dalmau <jdalmaudev@gmail.com>
2024-07-21 10:59:49 +0200

    Added dummy data

---
+++ devbudgetmanagement/src/assets/data/devs-data.ts
@@ -0,0 +1,32 @@
+export const DEVS_DATA = [
+  {
+    id: '1',
+    name: 'Cristian Cotes',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: '2',
+    name: 'Danitza Oduber',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: '3',
+    name: 'Luca Abdollahi',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: '4',
+    name: 'Jordi Dalmau',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: '5',
+    name: 'Carla Torrecillas',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: '6',
+    name: 'David Donaire',
+    avatar: 'user-6.jpg',
+  },
+];
