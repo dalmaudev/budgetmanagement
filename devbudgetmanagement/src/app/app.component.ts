aa8530c3b31e7eecb3ebaceb873c5eade9b37e06
Jordi Dalmau <jdalmaudev@gmail.com>
2024-07-21 10:58:59 +0200

    Initial Angular config

---
+++ devbudgetmanagement/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DeveloperComponent } from './developer/developer.component';
+import { DEVS_DATA } from '../assets/data/devs-data';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, DeveloperComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  devs = DEVS_DATA;
+  title = 'devbudgetmanagement';
+}
