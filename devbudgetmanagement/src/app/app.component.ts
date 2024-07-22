e64711b7ab803efe539adb77dda16cc45729acdb
Jordi Dalmau <jdalmaudev@gmail.com>
2024-07-22 08:32:38 +0200

    Added constructor data and methods

--- devbudgetmanagement/src/app/app.component.ts
+++ devbudgetmanagement/src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DeveloperComponent } from './components/developer/developer.component';
-import { DEVS_DATA } from '../assets/data/devs-data';
+import { DeveloperDetailComponent } from './components/developer-detail/developer-detail.component';
+import { DeveloperService } from './services/developer.service';
+import { Developer } from '../models/developer.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DeveloperComponent],
+  imports: [RouterOutlet, DeveloperComponent, DeveloperDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  devs = DEVS_DATA;
-  title = 'devbudgetmanagement';
+  appTitle = 'devbudgetmanagement';
+  selectedUserId = '';
+  devs!: Developer[];
+
+  constructor(private developer: DeveloperService) {
+    this.devs = this.developer.getDevsData();
+  }
+
+  get selectedDev() {
+    return this.devs.find((dev) => dev.id === this.selectedUserId)!;
+  }
+
+  onSelectDev(id: string) {
+    this.selectedUserId = id;
+  }
 }
